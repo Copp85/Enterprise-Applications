0238dbafa8e21d7c038ddac9bdfb07d776b22a7f
Shane Coppinger <S00190494@mail.itsligo.ie>
2019-02-17 15:18:53 +0000

    Layout_1

---
+++ EntAppSecond/Pages/FormWS.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntAppSecond.Pages
+{
+    public class FormWSModel : PageModel
+    {
+        [BindProperty]
+        public int Colour { get; set; }
+
+        [BindProperty]
+        public bool Consent { get; set; }
+
+        
+        public void OnGet()
+        {
+
+        }
+    }
+}
