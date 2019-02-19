79a23e2d33fb9318436a57cd03d202ecd061e627
Shane Coppinger <S00190494@mail.itsligo.ie>
2019-02-19 20:05:35 +0000

    Validation

---
+++ EntAppSecond/Model/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntAppSecond.Pages.Model
+{
+    
+
+    public class Student
+    {
+        [Required (ErrorMessage ="Please enter a Student ID")]
+        [Display(Name ="Student ID")]
+        public string StudentID { get; set; } = "";
+
+        [Required]
+        public string FirstName { get; set; } = "";
+
+        [Required]
+        public string LastName { get; set; } = "";
+    }
+}
---
+++ EntAppSecond/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntAppSecond.Pages.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntAppSecond.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        public Student Student { get; set; }
+        public void OnGet()
+        {
+
+        }
+    }
+}
